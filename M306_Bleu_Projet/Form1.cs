e452ab161fa786f5c17c516bb891682d5c87d04f
Nico <n.reymond.12@gmail.com>
2020-09-17 09:24:17 +0200

    [ADD] add form1.cs

--- M306_Bleu_Projet/Form1.cs
+++ M306_Bleu_Projet/Form1.cs
@@ -16,5 +16,10 @@ namespace M306_Bleu_Projet
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
